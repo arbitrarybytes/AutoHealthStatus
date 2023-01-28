5b25f7e3ea950b5650bb1efba5f7d1c90eb377ad
Vaibhav Sharma <arbitrarybytes@users.noreply.github.com>
2023-01-28 18:37:36 +0530

    Config changes

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,44 @@
-﻿namespace AutoHealthStatus;
+﻿using AutoHealthStatus.Models;
+
+namespace AutoHealthStatus;
 class Program
 {
+    const string ConfigFileName = "HealthCheckConfig.json";
+    static HealthCheckConfig _config = null;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if(!LoadConfig()) return;
+
+        ProcessConfigs();
     }
+
+    private static void ProcessConfigs()
+    {
+        $"Processing config started".LogAsInfo();
+
+        foreach(var config in _config.PortalConfigs)
+        {
+            $"[{config.Name} - {config.Authentication}] - Processing started".LogAsInfo();
+        }
+
+        $"Processing config started".LogAsSuccess();
+    }
+
+    static bool LoadConfig()
+    {
+        try
+        {
+            $"Loading health check config".LogAsInfo();
+            _config = System.Text.Json.JsonSerializer.Deserialize<HealthCheckConfig>(System.IO.File.ReadAllText(ConfigFileName));
+            $"Config loaded successfully with [{_config.PortalConfigs?.Count}] portals".LogAsSuccess();
+            return true;
+        }
+        catch (System.Exception ex)
+        {
+            $"Error loading configuration, please check the below path:\n{System.IO.Path.GetFullPath(ConfigFileName)}\nDetails: {ex.Message}".LogAsError();
+            return false;
+        }
+    }
+
 }
